c983ff8c3b0886ed216bea2a477f6ecac01629cd
Documenter.jl <documenter@juliadocs.github.io>
2025-11-24 03:37:55 +0000

    build based on 18a403b

---
+++ previews/PR4/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Documenter-PlantUML-plugin","page":"Description","title":"Documenter PlantUML plugin","text":"Simply add support of PlantUML diagrams to your documentation.","category":"section"},{"location":"#Usage","page":"Description","title":"Usage","text":"Create code block with type plantuml\n\nExample:\n\n```plantuml\nA -> B\n```\n\nResult:\n\n(Image: A->B)","category":"section"}]
+}
